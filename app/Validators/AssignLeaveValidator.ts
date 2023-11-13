d2de1a72c9bb8a957e4aafdc88f9180f7b25a4da
devwalex <salamiusmanolawale@gmail.com>
2023-11-13 15:51:52 +0100

    chore: update assign leave validator

--- app/Validators/AssignLeaveValidator.ts
+++ app/Validators/AssignLeaveValidator.ts
@@ -1,11 +1,20 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AssignLeaveValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    leaveTypeId: schema.string({ trim: true }),
+    leaveTypeId: schema.string({ trim: true },[
+      rules.uuid(),
+      rules.exists({
+        table: 'leave_types',
+        column: 'id',
+        where: {
+          is_deleted: false,
+        },
+      }),
+    ]),
     startDate: schema.string(),
     endDate: schema.string(),
     comments: schema.string.optional({ trim: true }),
@@ -13,6 +22,8 @@ export default class AssignLeaveValidator {
 
   public messages: CustomMessages = {
     'leaveTypeId.required': 'Leave Type is required.',
+    'leaveTypeId.uuid': 'Please select a valid leave type.',
+    'leaveTypeId.exists': 'Please select a valid leave type.',
     'startDate.required': 'Start Date is required.',
     'endDate.required': 'End Date is required.',
   }
